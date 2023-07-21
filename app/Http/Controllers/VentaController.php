a739963d255ec4bad8585667c85ae2cb59fbf45e
ydenv <ydenvalencia@gmail.com>
2023-07-21 13:08:29 -0500

    RegistroVenta save using stored procedure insertVenta

--- app/Http/Controllers/VentaController.php
+++ app/Http/Controllers/VentaController.php
@@ -54,27 +54,28 @@ class VentaController extends Controller
     public function store(Request $request)
     {
         $nuevoIdVenta = Venta::count() + 1;
-        $ultimoIdDetVenta = DetalleVenta::count();
         while(Venta::where('id_ven', $nuevoIdVenta)->exists()){
             $nuevoIdVenta++;
         }
-        $venta = new Venta();
+        
         $detalle_venta = [];
         $total_venta = 0;
         $fecha_venta = strtotime($request->input('fecha_ven'));
+
         foreach($request->input('detalle_venta') as $key => $item){
             $total_venta += $item['cantidad'] * $item['preciov'];
-            //Insert Detalle
-            $detVenta = new DetalleVenta();
-            $detVenta->id_detven = $ultimoIdDetVenta + ($key +1);
-            $detVenta->idproducto_detven = $item['id_producto'];
-            $detVenta->idventa_detven = $nuevoIdVenta; //Se agrega el id de venta manualmente, por que no hay relación en BD
-            $detVenta->precio_detven = $item['preciov'];
-            $detVenta->cantidad_detven = $item['cantidad'];
-            $detVenta->medidaprod_detven = 'ENTERO';
-            $detVenta->usercreated_detven = 'USR1';
-            $detalle_venta[] = $detVenta;
+            $detVenta = array(
+                'IdProducto' => (int)$item['id_producto'],
+                'IdVenta' => (int)$nuevoIdVenta,
+                'Precio' => (double)$item['preciov'],
+                'Cantidad' => (int)$item['cantidad'],
+                'MedidaProd' => 'ENTERO',
+                'UserCreated' => 'USR1',
+            );
+            array_push($detalle_venta, $detVenta);
         }
+
+        $venta = new Venta();
         $venta->id_ven = $nuevoIdVenta;
         $venta->idcliente_ven = $request->input('idcliente_ven');
         $venta->seriecomp_ven = $request->input('seriecomp_ven');
@@ -88,25 +89,45 @@ class VentaController extends Controller
         $venta->fecha_ven = date('Y/m/d', $fecha_venta);
         $venta->montoresto_ven = 0;
         $venta->formapago_ven = 'CONTADO';
-        $venta->numeropago_ven = '';
+        $venta->numeropago_ven = ' ';
         $venta->saldo_ven = 0;
         $venta->tipocambio_ven = 'PEN';
         $venta->valorcambio_ven = 0;
-        $venta->obs_ven = '';
-        $venta->estadofac_ven = 0;
+        $venta->obs_ven = ' ';
         $venta->usercreated_ven = "USR1";
 
-        DB::beginTransaction();
         try {
-            $venta->save();
-            $venta->detalle()->saveMany($detalle_venta);
-            DB::commit();
+            $row_count = 0;
+            DB::statement('call insertVenta(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)',
+                array(
+                    $venta->id_ven,
+                    $venta->idcliente_ven,
+                    $venta->numerocomp_ven,
+                    $venta->seriecomp_ven,
+                    $venta->tipocomp_ven,
+                    $venta->total_ven,
+                    $venta->subtotal_ven,
+                    $venta->igv_ven,
+                    $venta->valorigv_ven,
+                    $venta->dscto_ven,
+                    $venta->fecha_ven,
+                    $venta->montoresto_ven,
+                    $venta->formapago_ven,
+                    $venta->numeropago_ven,
+                    $venta->saldo_ven,
+                    $venta->tipocambio_ven,
+                    $venta->valorcambio_ven,
+                    $venta->obs_ven,
+                    $venta->usercreated_ven,
+                    json_encode($detalle_venta),
+                    $row_count,
+                ));
+
             return Response()->json([
                 'msg' => 'Se registró correctamente',
                 'code' => 200
             ]);
         } catch (\Exception $e) {
-            DB::rollback();
             return Response()->json([
                 'msg' => $e->getMessage(),
                 'code' => 500
